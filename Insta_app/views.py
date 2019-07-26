bb4bc08fd41b45bd31a17629388697c4a1bd78ab
Soniawangui <koisonia99@gmail.com>
2019-07-26 15:45:12 +0300

    Create a view function that will be responsible for showing the user

--- Insta_app/views.py
+++ Insta_app/views.py
@@ -5,3 +5,10 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
   return HttpResponse('Instagram!')
+
+def user_profile(request):  
+  current_user = request.user
+  profiles = Profile.objects.filter(user_id=current_user.id)[0:1]
+  posts = Image.objects.filter(user_id=current_user.id)
+
+  return render(request,'insta.html',{"profile_pic":profiles,"posts":posts})
