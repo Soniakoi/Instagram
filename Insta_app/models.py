764db8147d8042a1fb11847fd4c24afad0967179
Soniawangui <koisonia99@gmail.com>
2019-07-28 23:50:19 +0300

    Add models to my admin page

--- Insta_app/models.py
+++ Insta_app/models.py
@@ -8,14 +8,50 @@ class Profile(models.Model):
   bio = models.TextField()
   user = models.ForeignKey(User,on_delete=models.CASCADE)
 
+  def __str__(self):
+    return self.bio
+
+  def profile_save(self):
+    self.save()
+
+  def profile_delete(self):
+    self.delete()
+  class Meta:
+    ordering=['-bio']
+
+  @classmethod
+  def filter_by_username(cls,user_id):
+    user = cls.objects.filter(Profile__user=user.id)
+    return user
 
 class Image(models.Model):
-    image = models.ImageField(upload_to = 'image/',)
-    name = models.CharField(max_length=60)
-    poster = models.ForeignKey(User,on_delete=models.CASCADE, blank=True, related_name="images")
-    # location = models.ForeignKey(Location, blank=True)
+  image = models.ImageField(upload_to='image/',default='arusha.jpeg')
+  name = models.CharField(max_length=40)
+  # caption = HTMLField()
+  user = models.ForeignKey(User,on_delete=models.CASCADE)
+  likes = models.IntegerField(default=0)
     
-    # category = models.ForeignKey(Category,blank=True)
-    post_date = models.DateTimeField(auto_now_add=True)
-    comments= models.TextField(blank=True)
+  def __str__(self):
+    return self.name
 
+  def image_save(self):
+    self.save()
+  
+  def image_delete(self):
+    self.delete()
+
+  @classmethod
+  def search_user(cls,s_term):
+    result = cls.objects.filter(user__username__icontains=s_term)
+    return result
+
+class Comments(models.Model):
+  # comment = HTMLField()
+  user = models.ForeignKey(User,on_delete=models.CASCADE)
+  image = models.ForeignKey(Image,on_delete=models.CASCADE,null=True)
+
+  def __str__(self):
+    return self.comment
+
+  def comment_save(self):
+    self.save()
