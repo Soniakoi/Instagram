0f0df5263b3594a3901f651eac7649001c14cf53
Soniawangui <koisonia99@gmail.com>
2019-07-28 23:44:27 +0300

    Comment out some fields

--- Insta_app/models.py
+++ Insta_app/models.py
@@ -12,10 +12,10 @@ class Profile(models.Model):
 class Image(models.Model):
     image = models.ImageField(upload_to = 'image/',)
     name = models.CharField(max_length=60)
-    post = HTMLField()
     poster = models.ForeignKey(User,on_delete=models.CASCADE, blank=True, related_name="images")
-    location = models.ForeignKey(Location, blank=True)
-    category = models.ForeignKey(Category,blank=True)
+    # location = models.ForeignKey(Location, blank=True)
+    
+    # category = models.ForeignKey(Category,blank=True)
     post_date = models.DateTimeField(auto_now_add=True)
     comments= models.TextField(blank=True)
 
