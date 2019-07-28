68a8debbeef1605d1eceb6c7ffc58e485ec46fd9
Soniawangui <koisonia99@gmail.com>
2019-07-28 23:40:42 +0300

    Create an Image model which is going to store information of our images

--- Insta_app/models.py
+++ Insta_app/models.py
@@ -1,8 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 class Profile(models.Model):
   profile_pic = models.ImageField(upload_to='profile/')
   bio = models.TextField()
   user = models.ForeignKey(User,on_delete=models.CASCADE)
+
+
+class Image(models.Model):
+    image = models.ImageField(upload_to = 'image/',)
+    name = models.CharField(max_length=60)
+    post = HTMLField()
+    poster = models.ForeignKey(User,on_delete=models.CASCADE, blank=True, related_name="images")
+    location = models.ForeignKey(Location, blank=True)
+    category = models.ForeignKey(Category,blank=True)
+    post_date = models.DateTimeField(auto_now_add=True)
+    comments= models.TextField(blank=True)
 
