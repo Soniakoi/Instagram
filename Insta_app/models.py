f16e3065fccc2eb07824a3709c2305c6d10158a2
Soniawangui <koisonia99@gmail.com>
2019-07-28 23:24:44 +0300

    Import models from django.db,then create field objects that will define what sort of data our databases will store

--- Insta_app/models.py
+++ Insta_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+  profile_pic = models.ImageField(upload_to='profile/')
+  bio = models.TextField()
+  user = models.ForeignKey(User,on_delete=models.CASCADE)
+
