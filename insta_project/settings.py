17c0b07fb82cf19683402c24a97319d962f3c6d8
Soniawangui <koisonia99@gmail.com>
2019-07-28 22:21:23 +0300

    Create database and change database settings to use postgres

--- insta_project/settings.py
+++ insta_project/settings.py
@@ -78,7 +78,9 @@ WSGI_APPLICATION = 'insta_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'insta',
+        'USER': 'sonia',
+    'PASSWORD':'Sonia99',
     }
 }
 
@@ -107,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
